d48636cd1cd1b95ce679d974b52227e0f5e229d1
Ouzy Mariko <ousmane.mariko89@gmail.com>
2013-11-12 01:18:14 -0500

    sinatra

--- config/application.rb
+++ config/application.rb
@@ -23,3 +23,40 @@ module FridaySuit
     # config.i18n.default_locale = :de
   end
 end
+
+require 'sinatra/base'
+require 'sinatra/assetpack'
+
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__) # You must set app root
+
+  register Sinatra::AssetPack
+
+  assets {
+    
+  
+    serve '/images', from: 'app/images'    # Default
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    # The final parameter is an array of glob patterns defining the contents
+    # of the package (as matched on the public URIs, not the filesystem)
+    serve '/js', :from => 'assets/javascripts'
+    js :application, [
+      '/js/jquery.js',
+      '/js/app.js'
+      # You can also do this: 'js/*.js'
+    ]
+
+    serve '/css', :from => 'assets/stylesheets'
+    css :application, [
+      '/css/application.css',
+      '/css/component.css',
+      '/css/demo.css'
+  ]
+
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :simple   # :simple | :sass | :yui | :sqwish
+  }
+end
